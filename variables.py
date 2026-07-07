462d02e7c00f1bf9ebe6f83c2fc804c3ceeff6de
saikrishnanalamala <saikrishna.nalamala@gmail.com>
2026-07-07 16:21:01 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,92 @@
+Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+a=10
+print(a)
+10
+#variables
+print(7+8)
+15
+a=2
+b=4
+print(a+b)
+6
+c=30
+print(c)
+30
+z=20
+print(z)
+20
+_age=23
+print(_age)
+23
+name=saikrishna
+Traceback (most recent call last):
+  File "<pyshell#13>", line 1, in <module>
+    name=saikrishna
+NameError: name 'saikrishna' is not defined
+name="saikrishna"
+print(name)
+saikrishna
+city="vij"
+print(city)
+vij
+A2=40
+print(A2)
+40
+a0123456789=100
+print(0123456789)
+SyntaxError: leading zeros in decimal integer literals are not permitted; use an 0o prefix for octal integers
+print(a0123456789)
+100
+_=30
+print(_)
+30
+_d=20
+print(_d)
+20
+first_name="saikrishna"
+print(first_name)
+saikrishna
+firstname=saikrishna
+Traceback (most recent call last):
+  File "<pyshell#29>", line 1, in <module>
+    firstname=saikrishna
+NameError: name 'saikrishna' is not defined
+>>> firstname="saikrishna"
+>>> print(firstname)
+saikrishna
+>>> a=4;b=5
+>>> print(a+b)
+9
+>>> a=2,3,5,6,7,8,9
+>>> print(a)
+(2, 3, 5, 6, 7, 8, 9)
+>>> a,b,c,d=10,20,30,40
+>>> print(a,b,c,d)
+10 20 30 40
+>>> a,b,c=(10,20,30)
+>>> print(a,b,c)
+10 20 30
+>>> a=6
+>>> print(a)
+6
+>>> del a
+>>> print(a)
+Traceback (most recent call last):
+  File "<pyshell#43>", line 1, in <module>
+    print(a)
+NameError: name 'a' is not defined
+>>> fname="sai"
+>>> lname="krishna"
+>>> print(fname+lname)
+saikrishna
+>>> print(fname+""+lname)
+saikrishna
+>>> print(fname+" "+lname)
+sai krishna
+>>> Name="sai"
+>>> print(Name)
+sai
+>>> name="krishna"
+>>> print(name)
+krishna
