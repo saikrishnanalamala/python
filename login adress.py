03de46a8798624933071fdf7099d53299e160cea
saikrishnanalamala <saikrishna.nalamala@gmail.com>
2026-07-20 14:03:16 +0530

    Add files via upload

---
+++ login adress.py
@@ -0,0 +1,90 @@
+'''a=str(input("enter username"))
+b=int(input("enter password"))
+if a == "pooja":
+      print("enter to login")
+if b == 1234:
+      print("correct password")
+else:
+     print("enter invalid")'''
+
+
+'''username=input("enter the username")
+password=int(input("enter the password"))
+if username=="pooja":
+   if password==1234:
+       print("login succesful")
+else:
+           print("invaild credentials")'''
+
+
+
+
+
+'''username=input("enter the username")
+password=int(input("enter the password"))
+if username=="pooja"and password=="pooja@1234":
+    print("login successful")
+else:
+   print("invaild credentials")'''
+
+
+
+
+
+
+
+'''a=int(input("enter the age"))
+b=int(input("enter the marks"))
+c=int(input("enter the attendence"))
+if a>18:
+    print("eligible for vote")
+else:
+    print("not eligible for vote")
+if b>=70:
+    print("allow to write exams")
+if c>=80:
+    print("they are eligible for schorlship")
+else:
+    print("not eligible")'''
+
+
+
+'''name=str(input("enter the cake name"))
+if name=="redvalent":
+         print(1200)
+if name=="chocoalmond":
+         print(1000)
+elif name=="honey alomomd":
+        print(800)
+elif name=="butterscoth":
+        print(600)
+else:
+    print("it is not availble")'''
+
+
+
+
+'''name=str(input("enter the pizza name"))
+if name=="bbq pizza":
+    print(1200)
+elif name=="crispy chinken":
+    print(800)
+elif name=="panner pizza":
+    print(600)
+elif name=="corn pizza":
+    print(400)
+elif name=="frenchfries and coke":
+    print(200)'''
+
+
+
+
+
+
+
+
+
+
+
+
+            
