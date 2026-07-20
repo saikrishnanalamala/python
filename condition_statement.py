1f6cfd34973ad6f9ddeb68c4d1b0d2ad1d38160d
saikrishnanalamala <saikrishna.nalamala@gmail.com>
2026-07-20 14:01:39 +0530

    Add files via upload

---
+++ condition_statement.py
@@ -0,0 +1,107 @@
+'''a=15
+b=10
+if a>b:
+    print("true")'''
+
+
+'''a=12
+b=18
+if b>a:
+    print("false")'''
+
+'''a=12
+b=14
+if a<=b:
+    print("true")'''
+    
+'''a=5
+b=7
+if a<=b:
+    print("false")'''
+
+'''a=9
+if a>3:
+    print("greater")'''
+
+'''a=int(input("a value"))
+b=int(input("b value"))
+if a<b:
+      print("less")'''
+'''a=int(input("a value"))
+if a>40:
+    print("greater")'''
+
+
+#if_condition using logical operators
+#and,or ,not'''
+'''a=5
+b=7
+if a<b and b>a:
+    print("true")'''
+
+'''a=9
+b=11
+if a<=b and b>=a:
+    print("true")'''
+
+
+'''a=4
+b=6
+if a!=b and a==b:
+    print("false")'''
+
+#is,isnot
+'''a=7
+if type(a) is int:
+    print("it is not")'''
+
+
+'''a=9
+if type(a) is not int:
+    print("false")'''
+
+'''a=int(input("a value"))
+if type(a) is not int:
+    print("true")'''
+
+
+#if-condition by using membership operator
+#in,not in 
+
+'''a=2,4,5,6,7,8,9,10
+if 10 in a:
+    print("true")'''
+
+
+'''a=2,3,4,5,6,7,8,9,10
+if 10 in a:
+    print("true")'''#error
+
+'''a=[1,2,3,4,5,6,7,8]
+b=int(input("value"))
+if b in a:
+    print("true")'''
+
+#if-condition by using comparision operator
+'''a=3
+b=6
+if a<b:
+    print("less")
+else:
+    print("true")'''
+
+'''a=3
+b=6
+if a>b:
+     print("less")
+else:
+    print("true")'''
+           
+
+
+
+
+
+
+
+
