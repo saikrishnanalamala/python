8452f8e171c3578d08f9abb1280baa72799b8c1f
saikrishnanalamala <saikrishna.nalamala@gmail.com>
2026-07-24 19:33:27 +0530

    Add files via upload

---
+++ breakn continue,pass().py
@@ -0,0 +1,92 @@
+'''difference between break,continue,pass
+break is used to terminate the entire loop.
+continue is used to correct iteration and rest of code will continue.
+pass is a null statement it does nothing but synethically we need.'''
+
+#break()
+'''a=10
+while a>1:
+    print(a)
+    a=a-1
+    if a==6:
+        break'''
+
+'''for i in range(25):
+    if i==19:
+     break
+print(i)'''
+
+
+'''a="python"
+for i in a:
+    if i=="h":
+        break
+    print(i)'''
+    
+
+#continue()
+'''a=30
+while a>30:
+    print(a)
+    a=a-1
+    if a==15:
+     continue
+    print(a)'''
+
+
+
+'''for i in range(15):
+    if i==11:
+        continue
+    print(i)'''
+
+
+
+
+'''a="python"
+for i in a:
+    if i=="h":
+       continue
+    print(i)'''
+
+
+#pass()
+
+'''a=9
+while a>2:
+    print(a)
+    a=a-1
+    if a==7:
+        pass'''
+
+
+
+'''for i in range(25):
+    if i==20:
+        pass
+    print(i)'''
+
+
+
+                          
+
+
+                                         #ATM APPLICATION
+
+
+
+account=100000
+password=1234
+card="c"
+
+a=int(input())
+b=int(input())
+c=int(input())
+d=str(input("insert the card"))
+
+if a==b:
+    print("hello saikrishna welcome")
+if  b==c:
+    print("correct and check acc details")
+    
+ 
