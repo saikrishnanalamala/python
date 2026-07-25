2564c8e8ad207e5e242b44d9ae55f27f9d79aee7
saikrishnanalamala <saikrishna.nalamala@gmail.com>
2026-07-25 20:13:08 +0530

    Add files via upload

---
+++ bmi calculator.py
@@ -0,0 +1,38 @@
+                      #bmi calculator
+
+'''height=float(input("enter the height"))
+weight=float(input("enter the weight"))
+bmi=weight/(height)**2
+if bmi<=18.5:
+    print("under weight")
+elif bmi>18.5 and bmi<=24.5:
+     print("healthy weight")
+elif bmi>24.5 and bmi<=29.5:
+    print("over weight")
+elif bmi>30:
+    print("obesity")'''
+ 
+
+#patterns 
+
+'''n=5
+for i in range(1,6):
+    print("*"*i)'''
+
+'''n=int(input())
+for i in range(n,0,-1):
+    print("*"*i)'''
+
+
+'''rows=5
+for i in range(rows):
+    for j in range(rows):
+        print("*",end="")
+    print()'''
+
+
+'''a=4
+for i in range(1,a+1):
+ print("",a-i,"*"*i)'''
+
+
